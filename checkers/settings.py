a47d8b705f8a0aae20c680a2aeb1c2254d8b90aa
Illay Sasson <illaysasson5@gmail.com>
2021-08-31 22:15:37 +0300

    Added images for readme file

--- checkers/settings.py
+++ checkers/settings.py
@@ -1,5 +1,5 @@
-ai_depth = 5
-advanced_evalaute = True
-free_king_movement = True
-backwards_eating_in_doubles = False
-srufim = True
+ai_depth = 3 # How deep does the minimax algorithm go
+advanced_evalaute = True # Use the advanced formula for the minimax evaluating
+free_king_movement = True # Makes the king freely move on diagonals, and eat anything in its way (aka Russian/Israeli rules)
+backwards_eating_in_doubles = False # Gives pawns the ability to eat backwards in jumps with multiple captures. Currently not working
+srufim = False # A game rule that makes it mandatory to choose the move with the most captures
